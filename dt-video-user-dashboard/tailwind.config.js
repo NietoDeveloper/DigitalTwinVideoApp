49a98647cc7835cfde8ae6ab239ea0dc41fcf8b6
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-14 00:20:53 -0500

    Create tailwind.config.js

---
+++ dt-video-user-dashboard/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'space-black': '#050505',
+        'dt-gold': '#FFD700',
+        'dt-gainsboro': '#DCDCDC',
+        'dt-cyan': '#00c8ff',
+      },
+      backgroundImage: {
+        'grid-pattern': "linear-gradient(to right, #1a1a1a 1px, transparent 1px), linear-gradient(to bottom, #1a1a1a 1px, transparent 1px)",
+      }
+    },
+  },
+  plugins: [],
+};
