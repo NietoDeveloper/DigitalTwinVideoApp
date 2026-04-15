ccbf725655e32c02113b3205b886955fae6ab992
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-15 02:31:23 -0500

    up

--- dt-video-landing/tailwind.config.ts
+++ dt-video-landing/tailwind.config.ts
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,22 +9,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        // Colores oficiales de Software DT
         main: "#DCDCDC",
         gold: "#FFD700",
         yellowColor: "#FEB60D",
         headingColor: "#000000",
         textColor: "#000000",
       },
-      backgroundColor: {
-        'main': '#DCDCDC',
-        'card': '#FFFFFF',
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
   },
   plugins: [],
 };
+export default config;
