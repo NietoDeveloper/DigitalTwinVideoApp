ebb4916106a9dad10324b1f8814f90d68fb19813
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-15 23:35:16 -0500

    Update layout.tsx

--- dt-video-landing/src/app/layout.tsx
+++ dt-video-landing/src/app/layout.tsx
@@ -13,10 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en" className="scroll-smooth" suppressHydrationWarning>
+  children: React.Reactscroll-smooth" suppressHydrationWarning>
       <body 
   )
 }
