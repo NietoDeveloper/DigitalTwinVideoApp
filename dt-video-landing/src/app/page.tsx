bbe05bd99789d1f983de6930b0367deb0b12a87c
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-15 23:36:32 -0500

    Update page.tsx

--- dt-video-landing/src/app/page.tsx
+++ dt-video-landing/src/app/page.tsx
@@ -5,17 +5,7 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import Footer from "../components/Footer";
-
-export default function Home() {
-  return (
-    <main className="relative bg-main min-h-screen">
-      {/* 1. Navegación Fija - Z-index alto para estar sobre el video */}
-      <div className="relative z-50">
- 
-      </section>
-      
-      {/* 3. Sección de Información Técnica */}
-      <section className="relative z-10">
+"relative z-10">
         <About />
       </section>
       
