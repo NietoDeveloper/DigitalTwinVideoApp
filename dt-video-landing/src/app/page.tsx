330c6366892f6cddfe83c10169db2fc1ad13e5f2
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-15 23:36:39 -0500

    Update page.tsx

--- dt-video-landing/src/app/page.tsx
+++ dt-video-landing/src/app/page.tsx
@@ -2,15 +2,7 @@
 
 // Importaciones con rutas relativas para evitar cualquier conflicto de alias con Turbopack
 import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import About from "../components/About";
-import Footer from "../components/Footer";
-"relative z-10">
-        <About />
-      </section>
-      
-      {/* 4. Pie de Página */}
-      <Footer />
+import Hero from "../comp
     </main>
   );
 }
