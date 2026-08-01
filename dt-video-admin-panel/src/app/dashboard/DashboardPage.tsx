d9d33bd465b07e4cefee820865d6e88b1e1557bf
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-07-31 21:48:39 -0500

    Update DashboardPage.tsx

--- dt-video-admin-panel/src/app/dashboard/DashboardPage.tsx
+++ dt-video-admin-panel/src/app/dashboard/DashboardPage.tsx
@@ -157,5 +157,4 @@ export function DashboardPage() {
         </div>
 
         <div className="glass-card">
-          <h3 className="text-[10px] uppercase tracking-widest text-gray-500 font-bold">
-      
+          <h3 className="text-[10px] uppercase tracking-widest text-gray-500 font-bol
