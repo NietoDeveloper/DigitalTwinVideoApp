d238a0195f05b8268e74dc8e314aac41c43afb17
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-09-18 23:08:42 -0500

    Update page.tsx

--- dt-video-landing/src/app/page.tsx
+++ dt-video-landing/src/app/page.tsx
@@ -1,8 +1,3 @@
 "use client";
 
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import About from "../components/About";
-import Footer from "../components/Footer";
-
-export default function Home() 
+ 
