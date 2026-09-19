7ded19491dac5a2dc7671f51903506f09c38f7d2
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-09-18 23:19:13 -0500

    Update layout.tsx

--- dt-video-landing/src/app/layout.tsx
+++ dt-video-landing/src/app/layout.tsx
@@ -14,9 +14,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <html lang="en" className="scroll-smooth" suppressHydrationWarning>
-      <body 
         {children}
       </body>
     </html>
