8aa52a8b289cb72e8eaa05983c78346eff91b579
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-09-18 23:01:04 -0500

    Update page.tsx

--- dt-video-landing/src/app/page.tsx
+++ dt-video-landing/src/app/page.tsx
@@ -10,8 +10,7 @@ export default function Home() {
     <main className="relative bg-black min-h-screen w-full">
       {/* 1. Navegación - Siempre sobre el contenido */}
       <div className="fixed top-0 w-full z-50">
-        <Navbar />
-      </
+        <Navb      </
       
       oll */}
 
