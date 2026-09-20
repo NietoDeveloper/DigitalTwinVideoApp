b961b9c22d44bc138177be130641a6dbe8fe6fbe
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-09-19 22:58:07 -0500

    Update Hero.tsx

--- dt-video-landing/src/components/Hero.tsx
+++ dt-video-landing/src/components/Hero.tsx
@@ -8,12 +8,7 @@ const videos = [
   "https://v.ftcdn.net/02/10/35/33/700_F_210353392_8Z99h8lR6P7O0E0W9v5i2kR9uUuX6X3Y_ST.mp4",
   "https://v.ftcdn.net/05/57/02/76/700_F_557027663_vL2uVpWvYmZ9S8H1L7rK1p6u5vR4oU.mp4",
   "https://v.ftcdn.net/01/71/34/44/700_F_1713444
-  if (!isMounted) return <div className="h-screen bg-black" />;
-
-            playsInline
-            controls={false}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 0.6 0' }} className="
+  if (!isMounted) return <div className="h-screen bg
       </div>
     </section>
   );
