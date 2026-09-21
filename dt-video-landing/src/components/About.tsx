43e7a8c516e0ff4a7c00737f355db60fd14edd15
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-09-20 23:51:27 -0500

    Update About.tsx

--- dt-video-landing/src/components/About.tsx
+++ dt-video-landing/src/components/About.tsx
@@ -10,12 +10,7 @@ const About = () => {
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-px bg-black/5 border border-black/5">
               {specs.map((spec, index) => (
                 <div 
-             ion-colors duration-500 group"
-
-              <span>Ref: DT-VIDEO-2026</span>
-            </div>
-          </div>
-
+            
         </div>
       </div>
     </section>
