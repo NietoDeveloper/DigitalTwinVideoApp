5e7de12508cf240e007227087f0403dd957ba45b
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-09-20 23:50:40 -0500

    Update About.tsx

--- dt-video-landing/src/components/About.tsx
+++ dt-video-landing/src/components/About.tsx
@@ -12,9 +12,6 @@ const About = () => {
                 <div 
                   key={index} 
                   className="bg-main p-8 hover:bg-white transition-colors duration-500 group"
-                >
-ingColor uppercase">
-                                <p className="text-[9px] uppercase tracking-[0.3em] text-textColor/40 mb-3 group-hover:text-gold transition-colors">
 
                 </div>
               ))}
