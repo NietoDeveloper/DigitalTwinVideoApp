2de2fd492e5cdcad6d0adbcb9f2ba06d9f19e3d6
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-09-21 23:35:36 -0500

    Update Navbar.tsx

--- dt-video-landing/src/components/Navbar.tsx
+++ dt-video-landing/src/components/Navbar.tsx
@@ -90,7 +90,7 @@ export default function Navbar() {
                   className="flex items-center gap-4 text-lg font-bold text-white hover:text-gold transition-colorst-gold" />
                   <span className="uppercase tracking-widest text-sm">{link.name}</span>
                 
-              ))}
+          
               <button className="w-full bg-white text-black uppercase tracking-widest py-4 rounded-xl">
       
               </butt
