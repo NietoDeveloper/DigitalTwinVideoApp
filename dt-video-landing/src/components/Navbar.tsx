4eef4e771581e6d27920857a8472dde8eda0d056
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-09-21 23:50:37 -0500

    Update Navbar.tsx

--- dt-video-landing/src/components/Navbar.tsx
+++ dt-video-landing/src/components/Navbar.tsx
@@ -91,7 +91,7 @@ export default function Navbar() {
                   <span className="uppercase tracking-widest text-sm">{link.name}</span>
                 
           
-              <button classNameext-black uppercase tracking-widest py-4 rounded-x
+              <button classNameext-blat py-4 rounded-x
             
           </mo
     
