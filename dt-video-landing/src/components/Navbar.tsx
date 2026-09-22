548175ee7f92d1994ee7bd38c7c991f656d281e1
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-09-21 23:46:21 -0500

    Update Navbar.tsx

--- dt-video-landing/src/components/Navbar.tsx
+++ dt-video-landing/src/components/Navbar.tsx
@@ -92,8 +92,7 @@ export default function Navbar() {
                 
           
               <button className="w-full bg-white text-black uppercase tracking-widest py-4 rounded-x
-              </
-          
+            
           </motion
         )}
       </AnimatePres
