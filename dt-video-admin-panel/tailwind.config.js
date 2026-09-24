30446cd1f1eed531e2a9b4c6b41a6d27365bea0a
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-09-23 19:39:43 -0500

    Update tailwind.config.js

--- dt-video-admin-panel/tailwind.config.js
+++ dt-video-admin-panel/tailwind.config.js
@@ -10,4 +10,4 @@ export default {
         // Aquí puedes heredar los colores de tu branding Software DT
         gainsboro: "#DCDCDC",
         gold: "#FFD700",
-        yellowColor:
+        yellow
